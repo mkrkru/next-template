32899b696c164ab9e86ed04e0322d8000e59f21e
makarka <42581149+mkrkru@users.noreply.github.com>
2023-12-03 16:28:00 +0300

    upd

--- components/Common/Launcher.tsx
+++ components/Common/Launcher.tsx
@@ -3,6 +3,7 @@ import { setIsLaptop } from "@/redux/miscSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from '@/redux/hooks';
 import { motion } from 'framer-motion';
+import { ease } from "@/utils/misc";
 
 export function Launcher() {
     const dispatch = useDispatch();
@@ -17,7 +18,7 @@ export function Launcher() {
             style={{ width: '100vw', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 10000, background: 'black', pointerEvents: 'none' }}
             initial={{ opacity: 1 }}
             animate={{ opacity: isLaptop === null ? 1 : 0 }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.3, ease }}
         />
     </>
 }
